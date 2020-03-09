789e6577276a8c6ee270ece63223f8f86ef09ae2
Erick Mendizabal <erick.mendizabal@creative-works.us>
2020-03-09 14:44:57 -0600

    adjustments in the code and change of positions in some titles

--- landing-page-subscribe-save.php
+++ landing-page-subscribe-save.php
@@ -17,9 +17,11 @@ get_header();
 			<div class="banner-description" style="margin-bottom:0px;"><?php the_field('description_banner_lp'); ?></div><!--end banner-description-->
 		</div><!--end banner-r-->
 <div class="k-hero--action" style="text-align:center; width:100%; margin-top:40px;">
-            <a href="/cbd-gummies/" class="k-button k-button--primary">
-          Shop Softgels &nbsp; &#8594;
-        </a>
+<?php if(get_field('show_button_banner')){ ?>
+          <a href="<?php the_field('button_url_banner'); ?>" class="k-button k-button--primary">
+          	<?php the_field('button_title_banner'); ?> &nbsp; &#8594;
+        	</a>
+<?php } ?>
         </div>
 	</div><!--end k-inner k-inner--md-->
 </section><!--end banner-lp-->
@@ -62,7 +64,7 @@ get_header();
 <section id="sign-up-area" style="padding-top:2% !important;">
 	<div class="content-sign-up-area">
 		<h3 class="title-sign-up-section" style="text-align:center; margin:0px;">
-			<?php the_field('title_qa_section'); ?>
+			<?php the_field('text_boxes_area'); ?>
 		</h3><!--end title-sign-up-section-->
 		<div class="content-sign-up-section" style="display:flex; flex-wrap:wrap;">
 		<?php
@@ -80,7 +82,7 @@ get_header();
 <section id="sign-up-area" style="padding-top:2% !important;">
 	<div class="content-sign-up-area">
 		<div class="title-sign-up-section" style="text-align:center;">
-			<?php the_field('text_boxes_area'); ?>
+			<?php the_field('title_qa_section'); ?>
 		</div><!--end title-sign-up-section-->
 		<div class="content-sign-up-section">
 		<?php

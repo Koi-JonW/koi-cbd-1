74c020f5490ed2af2fb6a3d5b54878a478b893a9
sm.salvador@gmail.com <sm.salvador@gmail.com>
2020-04-17 14:13:15 -0500

    Update email referral scripts

--- template-my-rewards.php
+++ template-my-rewards.php
@@ -264,7 +264,15 @@ $user_email = $current_user->user_email;
         }
 
         var onError = function(err) {
-            alert('Oops! It looks like we\'re having trouble finding what you\'re looking for. Please try again later.');
+            if(err.message){
+                if(err.message == 'EMAILS_ALREADY_PURCHASED'){
+                    alert('Please enter a different email address.\nThis may not be a valid email address, or this person may already be signed up for Koi CBD Rewards.');
+                } else {
+                    alert('Oops! It looks like we\'re having trouble finding what you\'re looking for. Please try again later.');
+                }
+            } else {
+                alert('Oops! It looks like we\'re having trouble finding what you\'re looking for. Please try again later.');
+            }
             console.log('-- sendReferralEmails Error:\n', err);
         }
 

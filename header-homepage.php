566fec79f099ce0c67adbf79b15fff89bb2cb9a8
Bharti <dev@creative-works.us>
2020-08-14 17:16:28 +0530

    first

--- header-homepage.php
+++ header-homepage.php
@@ -129,6 +129,7 @@ footer .social-icons{max-width:175px; padding-left:15px; margin:0 auto; margin-t
 <script>
 var $ = jQuery.noConflict();
 $(document).ready(function(){
+    console.log("test");
     $("#k-nav-trigger").click(function() {
         $("html, body").animate({ scrollTop: 0 }, "fast");
         return false;
